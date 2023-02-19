deb9d431af75931c8492f4c782ebcb24468c5539
Christopher David <chris@arcade.city>
2023-02-19 12:56:21 -0600

    hmmmm

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,8 @@
 // import './bootstrap'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import Main from './Main'
+import { ConnectDemo } from './ConnectDemo'
+// import Main from './Main'
 
 const app = document.getElementById('root') as HTMLElement
-createRoot(app).render(<Main />)
-
-console.log('hello')
+createRoot(app).render(<ConnectDemo />)
