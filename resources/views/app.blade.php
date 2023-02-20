6d4732e1c00280fee26331e2740d0417f525f367
Christopher David <chris@arcade.city>
2023-02-20 12:43:03 -0600

    app layout favico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Arc</title>
+    <link rel="icon" href="/img/logo.png" type="image/png" sizes="16x16">
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/css/app.css'])
     @inertiaHead
