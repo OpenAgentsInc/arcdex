1ee32c5e5bdcadbc77f354c5fe1fe8be99a6bbbc
Christopher David <chris@arcade.city>
2023-02-20 12:17:20 -0600

    demo link broken

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -235,9 +235,9 @@
                 <div class="flex flex-col justify-center mt-16 px-0 sm:items-center sm:justify-center">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
-                            {{-- <a href="/chat" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                               Chat Demo
-                            </a> --}}
+                            <a href="/chat" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                               Chat Demo (broken for now)
+                            </a>
 
                             <a href="https://github.com/ArcadeLabsInc" target="_blank" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
                                 GitHub
