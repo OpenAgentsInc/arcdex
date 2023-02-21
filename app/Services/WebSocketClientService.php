7df1dffc5165632e14962198520b92f62c854eff
Christopher David <chris@arcade.city>
2023-02-21 12:06:12 -0600

    checkRelayForEvent works via websocket-php

--- app/Services/WebSocketClientService.php
+++ app/Services/WebSocketClientService.php
@@ -13,6 +13,30 @@ class WebSocketClientService
         $this->client = new WebSocketClient($uri);
     }
 
+    public function checkRelayForEvent($eventId = "c32251a74d641f0fdae5cd00fd0148fe82397db6b87d534431c248d268020f3d")
+    {
+        $subscriptionId = bin2hex(random_bytes(16));
+
+        $filters = [
+            "ids" => [$eventId],
+            "limit" => 1
+        ];
+
+        $requestMessage = ["REQ", $subscriptionId, $filters];
+
+        $this->client->send(json_encode($requestMessage));
+
+        $responseMessage = json_decode($this->client->receive(), true);
+
+        if ($responseMessage[0] === "EVENT" && $responseMessage[1] === $subscriptionId) {
+            $event = $responseMessage[2];
+            return $event;
+        } else {
+            return null;
+        }
+    }
+
+
     public function sendMessage($message)
     {
         $this->client->text($message);
