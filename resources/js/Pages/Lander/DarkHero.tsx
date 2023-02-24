376adee86c76378d9fd8a6914d016b7045dee9d6
Christopher David <chris@arcade.city>
2023-02-24 17:25:33 -0600

    icon

--- resources/js/Pages/Lander/DarkHero.tsx
+++ resources/js/Pages/Lander/DarkHero.tsx
@@ -66,6 +66,19 @@ export const DarkHero = () => {
                 What's new
               </span>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 opacity-75"
+                >
+                  <path
+                    strokeLinecap="round"
+                    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
                 <span>Building Arc #22: New Landing Page</span>
                 <ChevronRightIcon
                   className="h-5 w-5 text-gray-500"
