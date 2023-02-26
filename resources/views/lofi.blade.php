a8fcf20b0f238011e73f0145a0de9e685da19d1e
Christopher David <chris@arcade.city>
2023-02-26 16:25:10 -0600

    credit and bg and favicon

--- resources/views/lofi.blade.php
+++ resources/views/lofi.blade.php
@@ -1,6 +1,29 @@
+    <title>Arc</title>
+    <link rel="icon" href="/img/logo.png" type="image/png" sizes="16x16">
+
+<!-- pull in tailwind from cdn -->
+<script src="https://cdn.tailwindcss.com"></script>
+
+<div class="h-screen w-screen bg-gray-800 flex flex-col justify-center items-center">
+
 <!-- HTML audio player -->
 <audio controls>
   <source src="https://d22hdgrsmzgwgk.cloudfront.net/lofi/01%20Kupla%20-%20Eons%20%28master%29.mp3" type="audio/mp3">
     Your browser does not support the audio element.
 </audio>
 
+<div class="mt-6">
+  <div class="bg-gray-100 rounded-lg shadow-lg p-4">
+    <div class="flex flex-col md:flex-row items-center">
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12 rounded-full" src="/img/lofi.jpeg" alt="Lofi Girl">
+      </div>
+      <div class="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+        <p class="text-lg leading-tight font-semibold text-gray-900">Thank you Lofi Girl for the music!</p>
+        <p class="text-gray-600">All music from <a href="https://lofigirl.com" class="text-blue-500 hover:text-blue-600">Lofi Girl</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
