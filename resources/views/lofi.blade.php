1c5e25a68375ecde511f3460f1f8e2251e7829cd
Christopher David <chris@arcade.city>
2023-02-26 16:08:24 -0600

    First lofi embed

---
+++ resources/views/lofi.blade.php
@@ -0,0 +1,6 @@
+<!-- HTML audio player -->
+<audio controls>
+  <source src="https://d22hdgrsmzgwgk.cloudfront.net/lofi/01%20Kupla%20-%20Eons%20%28master%29.mp3" type="audio/mp3">
+    Your browser does not support the audio element.
+</audio>
+
