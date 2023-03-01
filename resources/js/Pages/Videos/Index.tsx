94b6dd0620662d8fb4d85a4957ad0bcd72206b2c
Christopher David <chris@arcade.city>
2023-03-01 11:05:59 -0600

    inertia video page noice

--- resources/js/Pages/Videos/Index.tsx
+++ resources/js/Pages/Videos/Index.tsx
@@ -10,12 +10,31 @@ export default function VideoIndex() {
         className="flex-1 relative z-0 overflow-y-auto focus:outline-none nice-scrollbar"
         tabIndex={0}
       >
-        <div className="mx-auto h-full">
-          <div className="p-12">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-              {videos.map((video: any) => {
-                return <Video video={video} key={video.id} />
-              })}
+        <div className="max-w-7xl mx-auto p-6 lg:p-8">
+          <div className="my-4 flex flex-col justify-center items-center">
+            <a href="/">
+              <img
+                src="/img/logo.png"
+                alt="logo"
+                className="w-auto bg-gray-100 dark:bg-gray-900"
+                style={{ height: 130 }}
+              />
+            </a>
+            <h1
+              className="font-bold text-2xl text-white mt-6"
+              style={{ color: 'rgb(116,65,244)' }}
+            >
+              Building Arc
+            </h1>
+          </div>
+
+          <div className="mx-auto h-full">
+            <div className="p-12">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                {videos.map((video: any) => {
+                  return <Video video={video} key={video.id} />
+                })}
+              </div>
             </div>
           </div>
         </div>
