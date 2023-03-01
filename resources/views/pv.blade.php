b0e9904c4eb7524a74dcbccbf31886e737d0793e
Christopher David <chris@arcade.city>
2023-03-01 11:14:40 -0600

    axe unused video model shaz, cleanup

--- resources/views/pv.blade.php
+++ resources/views/pv.blade.php
@@ -12,22 +12,6 @@
           class="dropzone"
           id="my-dropzone">
           @csrf
-          {{-- <div class="mb-4">
-            <label for="title" class="block text-gray-700 font-bold mb-2">Title:</label>
-            <input type="text" name="title" id="title" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label for="subtitle" class="block text-gray-700 font-bold mb-2">Subtitle:</label>
-            <input type="text" name="subtitle" id="subtitle" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label for="series_name" class="block text-gray-700 font-bold mb-2">Series Name:</label>
-            <input type="text" name="series_name" id="series_name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label for="episode_number" class="block text-gray-700 font-bold mb-2">Episode Number:</label>
-            <input type="number" name="episode_number" id="episode_number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div> --}}
         <input type="file" name="file"  style="display: none;">
     </form>
     <ul id="file-upload-list" class="list-unstyled">
