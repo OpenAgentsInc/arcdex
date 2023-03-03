711c5dd26e926c17aa40e134870ae3e3f830b758
Christopher David <chris@arcade.city>
2023-03-03 10:48:20 -0600

    clean up apiLogin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class LoginController extends Controller
@@ -23,21 +21,14 @@ class LoginController extends Controller
             'device_name' => 'required',
         ]);
 
-        // $user = User::where('email', $request->email)->first();
         $pubkey = $request->pubkey;
         $user = User::where('pubkey', $pubkey)->first();
-        // if no user, return error
         if (!$user) {
             // Create a user with this pubkey and log them in
             $user = User::create([
                 'pubkey' => $pubkey
             ]);
         }
-        // if (! $user || ! Hash::check($request->password, $user->password)) {
-        //     throw ValidationException::withMessages([
-        //         'email' => ['The provided credentials are incorrect.'],
-        //     ]);
-        // }
 
         return $user->createToken($request->device_name)->plainTextToken;
     }
