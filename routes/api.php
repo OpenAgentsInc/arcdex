428ca1e088b4b369e7020a2d24491910bc83c6d9
Christopher David <chris@arcade.city>
2023-03-06 10:23:35 -0600

    api routes cleanup

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ChannelController;
 use App\Http\Controllers\LoginController;
-use App\Models\Channel;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,19 +14,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/channels', [ChannelController::class, 'store']);
     Route::delete('/channels/{channel}/join', [ChannelController::class, 'leave']);
 });
-
-
-// Route::middleware('auth:sanctum')->delete('/channels/{channel}/join', function (Request $request, Channel $channel) {
-//     $channel->users()->detach($request->user()->id);
-
-//     return [
-//         'data' => [
-//             'id' => $channel->id,
-//             'title' => $channel->title,
-//             'joined' => false,
-//         ]
-//     ];
-// });
 
 Route::middleware('auth:sanctum')->get('/messages', function (Request $request) {
     $messages = Message::with('user')->get();
