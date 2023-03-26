a9f8417e072f5cb6f7c0b515f20a52b782996280
Christopher David <chris@arcadelabs.co>
2023-03-25 19:56:02 -0500

    vid55

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -364,6 +364,12 @@ class VideoController extends Controller
                 'title' => 'Chat Zaps, Part 3',
                 'subtitle' => "We integrate the NIP-57 Lightning Zaps spec into the Arc mobile app and send our first zaps to a few random Nostr users.",
                 "url" => 'https://d22hdgrsmzgwgk.cloudfront.net/uploads/arc54_5ece62b11c3df8c13d713847703cfa6b.mp4'
+            ],
+            [
+                'id' => 55,
+                'title' => 'Cross-Platform Zaps',
+                'subtitle' => 'We fix our desktop app and send NIP-57 zaps from web, mobile, and desktop.',
+                "url" => "https://d22hdgrsmzgwgk.cloudfront.net/uploads/arc55_43d7ccacf33764576c0620a282fc9c6f.mp4"
             ]
         ];
     }
